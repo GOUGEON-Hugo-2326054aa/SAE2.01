0a5c41b4676a9d29de3f059d9b20fbe132ced527
BuildTools <hugo.gougeon@etu.univ-amu.fr>
2024-06-05 23:27:50 +0200

    adaptation des boutons avec les image

--- src/main/java/com/exemple/sae201/FullViewController.java
+++ src/main/java/com/exemple/sae201/FullViewController.java
@@ -1,12 +1,11 @@
-//controller
 package com.exemple.sae201;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 
 public class FullViewController {
 
 }
+
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.example.sae201 {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires java.desktop;
+
+
     opens com.exemple.sae201 to javafx.fxml;
     exports com.exemple.sae201;
 }
